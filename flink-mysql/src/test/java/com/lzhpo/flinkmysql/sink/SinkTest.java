82e34a085cb3c4d35afe28fa7e3d865cf562ca72
Zhaopo Liu <zhaopo.liu@ext.deposit-solutions.com>
2020-07-28 23:30:34 +0800

    Delete some error test cases

--- flink-mysql/src/test/java/com/lzhpo/flinkmysql/sink/SinkTest.java
+++ flink-mysql/src/test/java/com/lzhpo/flinkmysql/sink/SinkTest.java
@@ -1,20 +1,10 @@
 package com.lzhpo.flinkmysql.sink;
 
-import com.google.gson.Gson;
 import com.lzhpo.common.modeltest.UserModelTest;
-import com.lzhpo.flinkkafka.config.KafkaConsumerConfig;
-import com.lzhpo.flinkkafka.source.FKConsumer;
 import com.lzhpo.flinkmysql.config.MysqlConnectionConfig;
-import org.apache.flink.api.common.functions.FlatMapFunction;
 import org.apache.flink.api.common.serialization.SimpleStringSchema;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
-import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-import org.apache.flink.util.Collector;
-import org.apache.kafka.clients.consumer.ConsumerRecord;
-
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 /**
  * 读取Kafka中指定的topic数据sink到MySQL
@@ -27,32 +17,16 @@ public class SinkTest {
     public static void main(String[] args) throws Exception {
         final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
-        // 添加数据源
-        DataStreamSource<ConsumerRecord<String, String>> consumerStreamSource =
-                env.addSource(
-                        new FKConsumer<>(
-                                new SimpleStringSchema(),
-                                KafkaConsumerConfig.builder()
-                                        .setBootstrapServers("192.168.200.109:9092")
-                                        .setGroupId("flink-consumer01-test")
-                                        .setEnableAutoCommit(false)
-                                        .setKeyDeserializer("org.apache.kafka.common.serialization.StringDeserializer")
-                                        .setValueDeserializer("org.apache.kafka.common.serialization.StringDeserializer")
-                                        .build(),
-                                // Java8使用Stream来创建传入Topic的Set集合
-                                Stream.of("flink-mysql-sink-topic").collect(Collectors.toSet())));
-
-        // transformation
-        SingleOutputStreamOperator<UserModelTest> flatMapStream = consumerStreamSource.flatMap(new FlatMapFunction<ConsumerRecord<String, String>, UserModelTest>() {
-            @Override
-            public void flatMap(ConsumerRecord<String, String> value, Collector<UserModelTest> collector) throws Exception {
-                UserModelTest user = new Gson().fromJson(value.value(), UserModelTest.class);
-                collector.collect(user);
-            }
-        });
+        DataStreamSource<UserModelTest> userModelTestDataStreamSource = env.fromElements(
+                UserModelTest.builder()
+                        .setId(123456787654L)
+                        .setName("lzhpo")
+                        .setLocation("广东广州")
+                        .build()
+        );
 
         // sink
-        flatMapStream.addSink(
+        userModelTestDataStreamSource.addSink(
                 new FlinkKafkaSinkMysql<>(
                         new SimpleStringSchema(),
                         MysqlConnectionConfig.builder()
--- flink-mysql/src/test/java/com/lzhpo/flinkmysql/source/SourceTest.java
+++ flink-mysql/src/test/java/com/lzhpo/flinkmysql/source/SourceTest.java
@@ -1,18 +1,10 @@
 package com.lzhpo.flinkmysql.source;
 
 import com.lzhpo.common.modeltest.UserModelTest;
-import com.lzhpo.flinkkafka.config.KafkaProducerConfig;
-import com.lzhpo.flinkkafka.sink.FKProducer;
 import com.lzhpo.flinkmysql.config.MysqlConnectionConfig;
-import org.apache.flink.api.common.functions.FlatMapFunction;
 import org.apache.flink.api.common.serialization.SimpleStringSchema;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
-import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-import org.apache.flink.util.Collector;
-
-import java.util.HashMap;
-import java.util.UUID;
 
 /**
  * 读取MySQL中的数据sink到Kafka
@@ -25,7 +17,7 @@ public class SourceTest {
     public static void main(String[] args) throws Exception {
         final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
-        DataStreamSource<UserModelTest> mysqlStream = env.addSource(
+        DataStreamSource<UserModelTest> userModelTestDataStreamSource = env.addSource(
                 new FlinkKafkaSourceMysql<>(
                         new SimpleStringSchema(),
                         MysqlConnectionConfig.builder()
@@ -36,31 +28,9 @@ public class SourceTest {
                 )
         );
 
-        SingleOutputStreamOperator<HashMap<String, String>> flatMapStream = mysqlStream.flatMap(new FlatMapFunction<UserModelTest, HashMap<String, String>>() {
-            @Override
-            public void flatMap(UserModelTest user, Collector<HashMap<String, String>> collector) throws Exception {
-                HashMap<String, String> map = new HashMap<>();
-                map.put(UUID.randomUUID().toString(), user.toString());
-                collector.collect(map);
-            }
-        });
-
-        flatMapStream.addSink(
-                new FKProducer<>(
-                        new SimpleStringSchema(),
-                        "flink-producer01-test",
-                        KafkaProducerConfig.builder()
-                                .setBootstrapServers("192.168.200.109:9092")
-                                .setAcks("all")
-                                .setRetries(0)
-                                .setBatchSize(16384)
-                                .setLingerMs(1)
-                                .setBufferMemory(33554432)
-                                .setKeySerializer("org.apache.kafka.common.serialization.StringSerializer")
-                                .setValueSerializer("org.apache.kafka.common.serialization.StringSerializer")
-                                .build()
-                )
-        );
+        userModelTestDataStreamSource
+                .print()
+                .setParallelism(2);
 
         env.execute("Flink mysql source");
     }
